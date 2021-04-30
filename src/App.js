4a8365979eeefca846735ea6aba0543885db23ee
LimitToInfinity <21stLimitless@gmail.com>
2021-04-30 10:39:18 -0600

    Queries to get all flowers and button to create new flower.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+const graphqlFetch = (query) => {
+  return fetch('http://localhost:9001/graphql', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    }).then(response => response.json());
+}
+
 function App() {
+
+  const [flowers, setFlowers] = useState([]);
+
+  useEffect(() => {
+    const flowersQuery = `{
+      flowers {
+        _id
+        kind
+        petals
+      }
+    }`
+
+    graphqlFetch(flowersQuery)
+      .then(({ data }) => setFlowers(data.flowers));
+
+  }, [])
+
+  const createFlower = () => {
+    const flowerName = "whatever"
+    const flowerPetals = 5
+    const createFlowerQuery = `mutation {
+      createFlower(flower: 
+        { kind: "${flowerName}", petals: ${flowerPetals} }
+      ) {
+        _id
+        kind
+        petals
+      }
+    }`
+
+    graphqlFetch(createFlowerQuery)
+      .then(({ data }) => setFlowers([...flowers, data.createFlower]));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +64,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={createFlower}>Create Flower</button>
       </header>
     </div>
   );
